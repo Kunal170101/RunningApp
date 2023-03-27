edd6aedb23a44da907ee7ba40ca2f90dbbc6ec76
kunal <kunal.dwarka@gmail.com>
2023-03-27 13:36:40 +0530

    Some bug fixes

--- app/src/main/java/com/androiddevs/runningappyt/ui/MainActivity.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+        bottomNavigationView.setOnNavigationItemReselectedListener { /* No-Op */ }
 
         navHostFragment.findNavController().addOnDestinationChangedListener { _, destination,_ ->
             when(destination.id){
